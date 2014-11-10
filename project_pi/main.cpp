341b32a1409217791670e6ed02d0484f11f07a9f
Lovachev S.S. VT-M14 <lovachevss@ismag2l.campus.iate.obninsk.ru>
2014-11-10 16:48:57 +0300

    Добавил лабу 2

--- project_pi/main.cpp
+++ project_pi/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
+#include <mpi.h>
 #include <stdlib.h>
 
 double randFunc(double min, double max){
@@ -11,12 +12,14 @@ double randFunc(double min, double max){
 
 int main(int argc, char **argv){
 
-  clock_t start = clock();
+  //clock_t start = clock();
   
   int N = 0;//число точек
   int Ncrc = 0;
   double x = 0, y = 0, Pi = 0;
   int radius = 1;
+  
+
   
   if(argc > 1){ 
     
@@ -26,18 +29,32 @@ int main(int argc, char **argv){
   }
   
   
+  MPI_Init (&argc, &argv);
   
-  for (int i = 0; i<N; i++){
+  int nproc, rank;
+  int summ = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  double t = MPI_Wtime();
+  
+  MPI_Comm_size(comm, &nproc);
+  MPI_Comm_rank(comm, &rank);
+  
+  
+  for (int i = rank; i<N; i+=nproc){
    
     x = randFunc(-(double)radius, (double)radius);
     y = randFunc(-(double)radius, (double)radius);
   
     if((x*x+y*y)<=(radius))
       Ncrc+=1;
+   
   }
+
+  printf("Nproc = %d\n", nproc);
   
-  Pi = 4.0*Ncrc/N;
-  
+  MPI_Reduce(&Ncrc, &summ, 1, MPI_DOUBLE, MPI_SUM, 0, comm);
+  Pi = 4.0*summ/N;
+/*  
   clock_t stop = clock();
   
   printf("\n");
@@ -46,10 +63,17 @@ int main(int argc, char **argv){
   }else{    
     printf("	Число PI = %12.9f\n", Pi);
   }
+*/  
+
+  printf("\n");
+  //printf("	Время выполнения программы = %f (сек)\n", (double)(stop-start)/CLOCKS_PER_SEC);
+      if(rank == 0){ 
+      printf("Pi = %12.9f\n", Pi);
+      printf("Time= %15.8g\n", MPI_Wtime()-t);
+    }
+  printf("\n");
   
-  printf("\n");
-  printf("	Время выполнения программы = %f (сек)\n", (double)(stop-start)/CLOCKS_PER_SEC);
-  printf("\n");
+  MPI_Finalize();
        
   return 0;
 }
